8024b0f3a04937a8642ebc2d9c5648e4f291d0db
Kurdi Szabolcs <x-Szabolcs.Kurdi@wizzair.com>
2017-03-31 15:11:23 +0200

    feat(backup): save uploaded json to backup dir

--- src/post.js
+++ src/post.js
@@ -1,13 +1,18 @@
-/* global cat:false */
+/* global cat:false, cp:false, mkdir:false */
 // uses upload.json
 require('shelljs/global');
+const shelljs = require('shelljs');
 const request = require('request-promise');
 const config = require('./config');
 const uri = `${config.api}/labels`;
 
 console.warn('TODO: overwrite does not work, delete first');
 
-let input = cat('upload.json');
+const backupDir = 'uploaded-jsons';
+const fileName = 'upload.json';
+const now = (new Date()).toISOString().substring(0, 19).replace('T', '_').replace(/:/g, '-');
+
+let input = cat(fileName);
 try {
   input = JSON.parse(input);
 } catch (err) {
@@ -19,9 +24,15 @@ try {
 for (let i = 0, l = input.length; i < l; i++) {
   const body = input[i];
   const id = Object.keys(body.Items)[0];
+  delete body.TaskId; // not yet implemented in the api
   request({method: 'POST', uri, body, json: true, rejectUnauthorized: false})
     .then((result) => {
       console.log(`Uploaded: ${i + 1}. (out of ${l}) - Result: ${JSON.stringify(result)}`);
+      shelljs.config.silent = true;
+      mkdir(backupDir);
+      let target = `${backupDir}/${now}_${fileName}`;
+      cp(fileName, target);
+      console.log(`File backed up to ${target}`);
     })
     .catch(err => {
       console.log(`Failed: ${i + 1}. [${id}] - Err: `, err.statusCode, err.statusMessage);
